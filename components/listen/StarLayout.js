9e117b0709bdf5098107d14732f5f291c75f621c
Abe <correodelamierdaaa@gmail.com>
2022-01-05 17:41:49 +0100

    fondo con mas estrellas

--- components/listen/StarLayout.js
+++ components/listen/StarLayout.js
@@ -14,12 +14,12 @@ function Stars({ position }) {
 		group.current.scale.set(s, s, s)
 	})
 	const [geo, mat, coords] = useMemo(() => {
-		const geo = new THREE.SphereBufferGeometry(1, 10, 10)
+		const geo = new THREE.SphereBufferGeometry(0.5, 0.5, 0.5)
 		const mat = new THREE.MeshBasicMaterial({
 			color: new THREE.Color("peachpuff"),
 			transparent: true,
 		})
-		const coords = new Array(1000)
+		const coords = new Array(5000)
 			.fill()
 			.map((i) => [
 				Math.random() * 800 - 400,
